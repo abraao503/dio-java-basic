80b7976fcbe2628e039ec2d8ba37a941019bd83f
abraao503 <abraao503@gmail.com>
2025-03-27 00:02:38 -0400

    feat: implement account creation and greeting functionality

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        numero = scanner.nextInt();
+
+        System.out.println("Digite a agência:");
+        agencia = scanner.next();
+
+        System.out.println("Digite o nome do cliente:");
+        nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo:");
+        saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
